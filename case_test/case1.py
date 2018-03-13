77d112e162d7b68b40f34c62b148c14f9fa35453
zhangyujing <zyj0704033@163.com>
2018-03-13 16:10:51 +0800

    change the loaddata method

--- case_test/case1.py
+++ case_test/case1.py
@@ -1,6 +1,8 @@
 #-*-conding:utf8-*-
 from matlab import engine
+import os
 eng = engine.start_matlab()
 eng.startup_matpower(nargout=0)
 eng.loaddata(nargout=0)
 eng.case1(nargout=0)
+os.system("pause")
--- case_test/loaddata.m
+++ case_test/loaddata.m
@@ -1,8 +1,15 @@
 clear
 run('startup_matpower.m')
-P_bt1=xlsread('26batou',2,'G119500:G120939');
-P_cf1=xlsread('27changfeng',2,'E74180:E75619');
-P_tsh1=xlsread('28taoshanhu',2,'E119210:E120649');
-P_dr1=xlsread('48derun',2,'E12596:E14035');
-P_sy1=xlsread('49shanyuan',2,'E91476:E92915');
+
+% P_bt1=xlsread('26batou',2,'G119500:G120939');
+% P_cf1=xlsread('27changfeng',2,'E74180:E75619');
+% P_tsh1=xlsread('28taoshanhu',2,'E119210:E120649');
+% P_dr1=xlsread('48derun',2,'E12596:E14035');
+% P_sy1=xlsread('49shanyuan',2,'E91476:E92915');
+load('P_bt1.mat');
+load('P_cf1.mat');
+load('P_dr1.mat');
+load('P_sy1.mat');
+load('P_tsh1.mat');
+
 disp('load finish')
